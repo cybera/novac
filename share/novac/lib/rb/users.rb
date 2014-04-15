10a397257251afc9783f0b00779ec74e0fb26506
Cybera Sysadmin <sysadmin@cybera.ca>
2014-04-15 16:41:50 -0600

    novac list-users now uses mysql2 as well

--- share/novac/lib/rb/users.rb
+++ share/novac/lib/rb/users.rb
@@ -19,11 +19,11 @@ def initialize
     master = novadb.master_cloud
     @users = {}
     begin
-      keystone = Mysql.new master[:server], master[:username], master[:password], 'keystone'
+      keystone = Mysql2::Client.new(:host => master[:server], :username => master[:username], :password => master[:password], :database => 'keystone')
 
       # Get the id and name of all users
       user_rs = keystone.query "select id, name from user"
-      user_rs.each_hash do |row|
+      user_rs.each do |row|
         @users[row['id']] = row['name']
       end
     ensure
@@ -67,13 +67,13 @@ def initialize
     master = novadb.master_cloud
     rows = []
     begin
-      keystone = Mysql.new master[:server], master[:username], master[:password], 'keystone'
+      keystone = Mysql2::Client.new( :host => master[:server], :username => master[:username], :password => master[:password], :database => 'keystone')
 
       projects = Projects.new
       @roles = {}
 
       role_rs = keystone.query "select id, name from role"
-      role_rs.each_hash do |row|
+      role_rs.each do |row|
           #Add _member_ id to ignore list
           if row['name'] == '_member_' && user_id == nil
             ignore_role_list << row['id']
@@ -92,7 +92,7 @@ def initialize
 
 
       #Obtain User Role Information
-      roles_rs.each_hash do |row|
+      roles_rs.each do |row|
         #Split up data section
         role_data = JSON.parse(row['data'])
         role_data["roles"].each do |role_row|
@@ -101,8 +101,6 @@ def initialize
         end
       end
 
-    ensure
-      keystone.close if keystone
     end
 
     headings = ['ID', 'Username', 'Project/Tenant', 'Role']
